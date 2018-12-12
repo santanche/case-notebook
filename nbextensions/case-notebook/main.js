5ed4272eedd4a7ab41fbf3ea5881ee0b2bf4d3a1
André Santanchè <santanche@gmail.com>
2018-12-12 08:14:18 -0200

    Rebuild the case.

--- nbextensions/case-notebook/main.js
+++ nbextensions/case-notebook/main.js
@@ -9,7 +9,7 @@ define([
 	    'components/marked/lib/marked',
 	    'base/js/events',
 	    'notebook/js/textcell'
-	], function(IPython, $, requirejs, cell, security, marked, events, textcell) {
+	], function(_IPython, _$, _requirejs, _cell, _security, _marked, events, _textcell) {
 
 	var notebookCell;
 	var mdhtml;
@@ -18,69 +18,70 @@ define([
 	var knotImage = "";
 
 	var marks = [
-		// /^(?:<p>)?[ \t]*==*[ \t]*(\w[\w \t]*)=*[ \t]*(?:\(([\s\w=-\+\*\/,])\))?(?:<\/p>)?/igm,
-		/^(?:<p>)?[ \t]*==*[ \t]*(\w[\w \t]*)(?:\(([\w \t]*)\))?[ \t]*=*[ \t]*(?:<\/p>)?/igm,           // knot
-        /\+\+[ \t]*([^-&<> \t][^-&<>\n\r\f]*)?(?:-(?:(?:&gt;)|>)[ \t]*(\w[\w. \t]*))?/igm, // option
+        /^(?:<p>)?[ \t]*==*[ \t]*(\w[\w \t]*)(?:\(([\w \t]*)\))?[ \t]*=*[ \t]*(?:<\/p>)?/igm,  // knot
+        /\+\+[ \t]*([^-&<> \t][^-&<>\n\r\f]*)?(?:-(?:(?:&gt;)|>)[ \t]*(\w[\w. \t]*))?/igm,     // option
         /-(?:(?:&gt;)|>) *(\w[\w. ]*)/igm,                       // divert
-        /^(?:<p>)? *: *([\w]+) *: *(?:<\/p>)?/igm,               // character
+        /^(?:<p>)?[ \t]*:[ \t]*([\w]+)[ \t]*:[ \t]*(?:<\/p>)?/igm,               // character
         /<img src="([\w:.\/\?&#\-]+)" (?:alt="([\w ]+)")?>/igm,  // image
-        /\{([\w ]*)(?:=([\w %]*)(?:\/([\w %]*))?)?\}(?:\(([\w ]*)(?:=([\w %]*)(?:\/([\w %]*))?)?\))?/igm // domain
+        /\{[ \t]*\?(\d+)?([\w \t]*)(?:\:([\w \t%]*))?\}/igm,       // user input
+		    /\{([\w \t]*)(?:=([\w \t%]*)(?:\/([\w \t%]*))?)?\}(?:\(([\w \t]*)(?:=([\w \t%]*)(?:\/([\w \t%]*))?)?\))?/igm, // domain
+		    /^(?:<p>)?[ \t]*~[ \t]*([\+\-=\*\\%]?)[ \t]*(\w*)?[ \t]*(\w+)[ \t]*(?:<\/p>)?/igm,               // score
     ];
 	
 	var render_cell = function(cell) {
-        notebookCell = cell;
-        mdhtml = cell.element.find('div.text_cell_render')[0];
+    notebookCell = cell;
+    mdhtml = cell.element.find('div.text_cell_render')[0];
         
 		var mdtext = mdhtml.innerHTML;
 		var mdinterface = mdhtml.innerHTML;
 
-        notebookCell.notebook.kernel.execute("HealthDM.clearTerms()");
+    notebookCell.notebook.kernel.execute("HealthDM.clearTerms()");
 
 		generateInterface(mdinterface);
         
-        var markFs = [markKnot, markOption, markDivert, markCharacter, markImage, markDomain];
+    var markFs = [markKnot, markOption, markDivert, markCharacter, markImage, markInput, markDomain, markScore];
         
-        // indexing knots
-        var knotContext = null;
-        var knotHeads = mdtext.match(marks[0]);
-        for (kh in knotHeads) {
-        	var label = knotHeads[kh].match(/==*[ \t]*(\w[\w \t]*)(?:\([\w \t]*\))?[ \t]*=*/i);
-        	label = label[1].trim();
-        	if (knotHeads[kh].indexOf("==") >= 0)
-        		knotContext = label;
-        	else
-        		label = (label.indexOf(".") < 0 && knotContext == null) ? label : knotContext + "." + label;
-        	knots.push(label);
-        }
-        
-        // replacing Ink marks
-        var mdfocus = mdtext;
-        var mdresult = "";
-        
-        var matchStart;
-        do {
-	        matchStart = -1;
-	        var selected = -1;
-	        for (mk in marks) {
-	        	var pos = mdfocus.search(marks[mk]);
-	        	if (pos > -1 && (matchStart == -1 || pos < matchStart)) {
-	        		selected = mk;
-	        		matchStart = pos;
-	        	}
-	        }
-	        
-	        if (matchStart > -1) {
-	        	var matchSize = mdfocus.match(marks[selected])[0].length;
-	        	var toReplace = mdfocus.substring(0, matchStart + matchSize);
-	        	mdresult += toReplace.replace(marks[selected], markFs[selected]);
-	        	if (matchStart + matchSize >= mdfocus.length)
-	        		matchStart = -1;
-	        	else
-	        		mdfocus = mdfocus.substring(matchStart + matchSize);
-	        }
-        } while (matchStart > -1);
-        
-        mdresult += mdfocus;
+		// indexing knots
+		var knotContext = null;
+		var knotHeads = mdtext.match(marks[0]);
+		for (kh in knotHeads) {
+			var label = knotHeads[kh].match(/==*[ \t]*(\w[\w \t]*)(?:\([\w \t]*\))?[ \t]*=*/i);
+			label = label[1].trim();
+			if (knotHeads[kh].indexOf("==") >= 0)
+				knotContext = label;
+			else
+				label = (label.indexOf(".") < 0 && knotContext == null) ? label : knotContext + "." + label;
+			knots.push(label);
+		}
+
+		// replacing Ink marks
+		var mdfocus = mdtext;
+		var mdresult = "";
+
+		var matchStart;
+		do {
+			matchStart = -1;
+			var selected = -1;
+			for (mk in marks) {
+				var pos = mdfocus.search(marks[mk]);
+				if (pos > -1 && (matchStart == -1 || pos < matchStart)) {
+					selected = mk;
+					matchStart = pos;
+				}
+			}
+			
+			if (matchStart > -1) {
+				var matchSize = mdfocus.match(marks[selected])[0].length;
+				var toReplace = mdfocus.substring(0, matchStart + matchSize);
+				mdresult += toReplace.replace(marks[selected], markFs[selected]);
+				if (matchStart + matchSize >= mdfocus.length)
+					matchStart = -1;
+				else
+					mdfocus = mdfocus.substring(matchStart + matchSize);
+			}
+		} while (matchStart > -1);
+
+		mdresult += mdfocus;
 
 		mdhtml.innerHTML = mdresult;
 	};
@@ -88,21 +89,27 @@ define([
 	var generateInterface = function(mdinterface) {
 		var knotBlocks = mdinterface.split(marks[0]);
 
+		/*
 		console.log(knotBlocks);
-		console.log('HealthDM.interfaceMain("index","' + knotBlocks[1].trim() + '","""' +
-		             knotBlocks[3] + '""","' + knotBlocks[1].trim().replace(/ /igm, "_") + '")');
-		notebookCell.notebook.kernel.execute(
-				'HealthDM.interfaceMain("index","' + knotBlocks[1].trim() + '","""' +
-	             knotBlocks[3] + '""","' + knotBlocks[1].trim().replace(/ /igm, "_") + '")');
+		*/
 
-        // var knotTemplate = (knotBlocks[2] == null) ? "presentation" : knotBlocks[2].trim().replace(" ", "_");
+	  var caseTitle = knotBlocks[1].trim();
+		var caseDescription = knotBlocks[3].replace(marks[1], "").replace(marks[4], "");  // marks - option/image
+		var caseImage = knotBlocks[3].match(marks[4])[0].replace(">", " style='width:100px'>");  // marks - image
+
+		console.log('HealthDM.interfaceMain("' + caseTitle + '","""' +
+	             caseDescription + '""","""' + caseImage + '""","' + caseTitle.replace(/ /igm, "_") + '")');
+		notebookCell.notebook.kernel.execute(
+				'HealthDM.interfaceMain("' + caseTitle + '","""' + caseDescription + '""","""' +
+	             caseImage + '""","' + caseTitle.replace(/ /igm, "_") + '")');
+
 		for (kb = 1; kb < knotBlocks.length; kb += 3) {
 			var pageName = knotBlocks[kb].trim().replace(/ /igm, "_");
 			knotTemplate = 
-				(knotBlocks[kb+1] == null) ? ((kb == 1) ? "presentation" : "knot") : knotBlocks[kb+1].trim().replace(" ", "_");
+				(knotBlocks[kb+1] == null) ? "knot" : knotBlocks[kb+1].trim().replace(" ", "_");
 			knotImage = "";
-			var pageContent = knotBlocks[kb+2].replace(marks[5], interfaceDomain).replace(marks[1], interfaceOption).
-			                  replace(marks[4], interfaceImage);
+			var pageContent = knotBlocks[kb+2].replace(marks[5], interfaceInput).replace(marks[6], interfaceDomain).
+			                  replace(marks[1], interfaceOption).replace(marks[4], interfaceImage).replace(marks[7], interfaceScore);
 			console.log('HealthDM.interfaceKnot("' + knotTemplate + '","' + pageName + '","' + knotBlocks[kb].trim() +
 					  '","""' + pageContent + '""","' + knotImage + '")');
 			notebookCell.notebook.kernel.execute(
@@ -111,31 +118,55 @@ define([
 		}
 	};
     
-    var interfaceDomain = function(matchStr, insideDescription, insideDetail1, insideRate1,
-    		                                 insideHeading, insideDetail2, insideRate2) {
-		return matchStr.match(/\{([\w= %\/]*)\}/i)[1];
-	}
+  var interfaceInput = function(_matchStr, insideRows, insideVariable, insideVocabulary) {
+		var variable = insideVariable.trim().replace(/ /igm, "_");
 
-    var interfaceOption = function(matchStr, insideText, insideDivert) {
+		var rows = (insideRows == null) ? 0 : parseInt(insideRows);
+		
+	  return ((rows > 0) ? "<textarea style='width:100%' rows=" + rows : "<input type='text'") +
+			     " class='userInput' id='" + variable +
+				   "' oninput=\"followInput('" + variable + "','" + insideVocabulary.trim() + "')\" " +
+				   "' onchange=\"evaluateInput('" + variable + "','" + insideVocabulary.trim() + "')\">" +
+	         ((rows > 0) ? "</textarea>" : "</input>");
+	};
+
+  var interfaceDomain = function(matchStr, _insideDescription, _insideDetail1, _insideRate1,
+    		                                   _insideHeading, _insideDetail2, _insideRate2) {
+		return matchStr.match(/\{([\w= %\/]*)\}/i)[1];
+	};
+
+  var interfaceOption = function(_matchStr, insideText, insideDivert) {
 		var display = (insideText != null) ? insideText : insideDivert;
 		
-        var link = (insideDivert != null) ? insideDivert : insideText;
+    var link = (insideDivert != null) ? insideDivert : insideText;
         		
-        link = link.trim().replace(/ /igm, "_");
+    link = link.trim().replace(/ /igm, "_");
 
-		return "<div class='case_link'><a href='" + link + ".html'>" + display + "</a></div>";
+		return "<div class='case_link'><a href='" + link + ".html' onclick=\"computeLink('" +
+		       link + "')\">" + display + "</a></div>";
 	};
 	
-	var interfaceImage = function(matchStr, insideSrc, insideAlt) {
+	var interfaceImage = function(matchStr, insideSrc, _insideAlt) {
 		if (knotImage === "")
 			knotImage = insideSrc;
 		
     	return matchStr.replace(">", " style='float:left' width='300px'>");
-    };
+  };
     
-   var markKnot = function(matchStr, insideText, insideTemplate) {
-	    // var variables = insideVariables.replace(/(?:\( *(\w*) *(?:([=-\+\*\/])([\w ]*))?\))?/igm);
-	   
+	var interfaceScore = function(_matchStr, insideSymbol, insideValue, insideVariable) {
+	  var symbol = (insideSymbol == null || insideSymbol.trim().length == 0) ? "=" : insideSymbol.trim();
+
+	  var output = "";
+
+	  if (symbol == "%")
+	     output = "<p class='case_text' id='var-" + insideVariable.trim() + "'></p><script>showScore('" + insideVariable.trim() + "')</script>";
+	  else
+		   output = "<script>computeScore('" + symbol + "', '" + insideVariable.trim() + "', '" + insideValue.trim() + "')</script>";
+		
+		return output;
+  };
+    
+  var markKnot = function(matchStr, insideText, _insideTemplate) {
     	var label = insideText.trim();
 
     	var display = matchStr.match(/==* *(\w[\w ]*)=*/ig);
@@ -146,24 +177,19 @@ define([
     		label = (label.indexOf(".") < 0 && knotContext == null) ? label : knotContext + "." + label;
     	
         return "<h1><a id='knot_" + label + "'><span style='font-style:italic'>" + display + "</span></a></h1>";
-    };
+  };
 
-    var markOption = function(matchStr, insideText, insideDivert) {
-    	/*
-    	console.log("inside text: " + insideText);
-    	console.log("inside divert: " + insideDivert);
-    	*/
-    	
+  var markOption = function(_matchStr, insideText, insideDivert) {
     	var display = (insideText != null) ? insideText : insideDivert;
     	var label = (insideDivert != null) ? insideDivert.trim() : insideText.trim();
     	var newLabel = divertResolver(label);
-        display = (newLabel != null) ? display : display + "-> ?" + label + "?";
-        link = "#knot_" + ((newLabel != null) ? newLabel : label);
+      display = (newLabel != null) ? display : display + "-> ?" + label + "?";
+      link = "#knot_" + ((newLabel != null) ? newLabel : label);
 
     	return "<ul><li><a href='" + link + "'><span style='font-weight:bold'>" + display + "</span></a></li></ul>";
-    };
+  };
     
-    var markDivert = function(matchStr, inside) {
+  var markDivert = function(_matchStr, inside) {
     	var label = inside.trim();
     	var newLabel = divertResolver(label);
     	
@@ -171,9 +197,9 @@ define([
     	var link = (newLabel != null) ? newLabel : label;
     	
     	return "<a href='#knot_" + link + "'><span style='font-weight:bold'>" + display + "</span></a>";
-    };
+  };
     
-    var divertResolver = function(label) {
+  var divertResolver = function(label) {
     	var newLabel = null;
     	
     	if (knots.indexOf(label) >= 0)
@@ -182,26 +208,35 @@ define([
     		newLabel = knotContext + "." + label;
     	
     	return newLabel;
-    };
+  };
     
-    var markCharacter = function(matchStr, inside) {
-    	var label = inside.trim();
-    	
+  var markCharacter = function(matchStr, _inside) {
     	var display = matchStr.replace(/: *([\w]+ *:)/igm, "<span style='font-weight:bold'>$1</span>");
     	
     	return display;
-    };
+  };
 
-    var markImage = function(matchStr, inside1, inside2) {
+  var markImage = function(matchStr, _inside1, _inside2) {
     	return matchStr.replace(">", " style='float:left' width='300px'><p style='clear:both'></p>");
-    };
-    
-    var meshBack = function(meshResult){
+  };
+
+  var markInput = function(_matchStr, insideRows, _insideVariable, _insideVocabulary) {
+		// var variable = insideVariable.trim().replace(/ /igm, "_");
+
+		var rows = (insideRows == null) ? 0 : parseInt(insideRows);
+		
+	  return ((rows > 0) ? "<textarea style='width:100%' rows=" + rows + "></textarea>"
+	                     : "<input type='text'></input>");
+	}
+
+  var markScore = function(matchStr, _insideSymbol, _insideValue, _insideVariable) {
+    	return "<p>" + matchStr + "</p>";
+  };
+
+  var meshBack = function(meshResult){
     	// alert("MeSH back: " + meshResult.content.data["text/plain"]);
     	
     	var regexR = /u?'#mesh_heading#([\w\s]*)#tree_number#([\w\:\/\.]*)'/igm;
-    	
-    	// console.log(meshResult);
     	
     	if (meshResult.msg_type == "execute_result") {
     		var result = meshResult.content.data["text/plain"];
@@ -216,21 +251,10 @@ define([
     		console.log("Sparql error: " + meshResult.content.evalue);
     	else
     		console.log("Unknown message return type: " + meshResult.msg_type);
-    	
-    	// alert("heading: " + heading + "; code: " + code);
-    };
+  };
     
-    var markDomain = function(matchStr, insideDescription, insideDetail1, insideRate1,
-    		                            insideHeading, insideDetail2, insideRate2) {
-        /*
-		console.log("inside description: " + insideDescription);
-    	console.log("inside detail 1: " + insideDetail1);
-    	console.log("inside rate 1: " + insideRate1);
-    	console.log("inside heading: " + insideHeading);
-    	console.log("inside detail 2: " + insideDetail2);
-		console.log("inside rate 2: " + insideRate2);
-		*/
-		
+  var markDomain = function(_matchStr, insideDescription, insideDetail1, insideRate1,
+   		                      insideHeading, insideDetail2, insideRate2) {
     	var description = insideDescription.trim();
     	var detail1 = (insideDetail1 != null) ? insideDetail1.trim() : "#";
     	var rate1 = (insideRate1 != null) ? insideRate1.trim() : "#";
@@ -238,68 +262,23 @@ define([
     	var detail2 = (insideDetail2 != null) ? insideDetail2.trim() : "#";
     	var rate2 = (insideRate2 != null) ? insideRate2.trim() : "#";
 
-		console.log("inside description: " + description);
-		console.log("inside heading: " + heading);
-		
-        notebookCell.notebook.kernel.execute(
+  		notebookCell.notebook.kernel.execute(
         		"HealthDM.findMeshCode('" + heading + "','" + detail1 + "','" + rate1 + "','" +
         		                            description + "','" + detail2 + "','" + rate2 + "')",
         		{iopub : {output: meshBack}},
         	    {silent: false, store_history : false, stop_on_error: false});
-
+    		
     	return "<a href='#mesh_addr#" + heading + "#' title='#tree_number#" + heading +
-    	                "#' target='_blank'>" + description + "</a>";
-    };
+    	         "#' target='_blank'>" + description + "</a>";
+  };
 
-    var load_ipython_extension = function() {
-        // alert("Observer installed");
-        
-        // Select the node that will be observed for mutations
-        // var targetNode = document.getElementById("notebook-container");
-    	
-        events.on("rendered.MarkdownCell", function (event, data) {
+  var load_ipython_extension = function() {
+        events.on("rendered.MarkdownCell", function (_event, data) {
             render_cell(data.cell);
         });
-        
-        
-        /*
-        var callbackEvent = function(event) {
-        	console.log("Event: " + event.detail);
-        };
-        
-        // targetNode.addEventListener("rendered.MarkdownCell", callbackEvent);
-        
-        var installEvent = function(node) {
-        	node.addEventListener("rendered.MarkdownCell", callbackEvent);
-        };
-        
-        document.querySelectorAll(".MarkdownCell *").forEach(installEvent);
-        
-        
-        // Options for the observer (which mutations to observe)
-        var config = { attributes: true, childList: true, subtree: true };
+  };
 
-        // Callback function to execute when mutations are observed
-        var callback = function(mutationsList, observer) {
-            for(var mutation of mutationsList) {
-                if (mutation.type == 'childList') {
-                    console.log("Observer: A child node has been added or removed.");
-                }
-                else if (mutation.type == 'attributes') {
-                    console.log('The ' + mutation.attributeName + ' attribute was modified.');
-                }
-            }
-        };
-
-        // Create an observer instance linked to the callback function
-        var observer = new MutationObserver(callback);
-
-        // Start observing the target node for configured mutations
-        observer.observe(targetNode, config);
-        */
-    };
-
-    return {
+  return {
         load_ipython_extension: load_ipython_extension
-    };
+  };
 });
